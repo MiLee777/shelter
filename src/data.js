9f8d659a0ea32c4c3ed2ee9c7e1370347ce6f004
MiLee777 <midyashka1506@gmail.com>
2024-06-23 09:42:11 +0700

    feat: add data about pets

---
+++ src/data.js
@@ -0,0 +1,120 @@
+export const Pets = () => [
+
+  {
+    name: 'Katrine',
+    src: './assets/image/our-pets/Katrine.png',
+    breed: 'Cat - British Shorthair',
+    text: 'Scarlett is a happy, playful girl who will make you laugh and smile. She forms a bond quickly and will make a loyal companion and a wonderful family dog or a good companion for a single individual too since she likes to hang out and be with her human.',
+    info: {
+      age: '2 years old',
+      inoculations: 'all',
+      diseases: 'none',
+      parasites: 'none'
+    }
+  },
+
+  {
+    name: 'Jennifer',
+    breed: 'Dog - Labrador',
+    src: './assets/image/our-pets/Jennifer.png',
+    text: 'Jennifer is a sweet 2 months old Labrador that is patiently waiting to find a new forever home. This girl really enjoys being able to go outside to run and play, but won`t hesitate to play up a storm in the house if she has all of her favorite toys.',
+    info: {
+      age: '2 months',
+      inoculations: 'none',
+      diseases: 'none',
+      parasites: 'none'
+    }
+  },
+
+  {
+    name: 'Woody',
+    breed: 'Dog - Golden Retriever',
+    src: './assets/image/our-pets/Woody.png',
+    text: 'Woody is a handsome 3 1/2 years old boy. Woody does know basic commands and is a smart pup. Since he is on the stronger side, he will learn a lot from your training. Woody will be happier when he finds a new family that can spend a lot of time with him.',
+    info: {
+      age: '3.5 years old',
+      inoculations: 'all',
+      diseases: 'none',
+      parasites: 'none'
+    }
+  },
+
+  {
+    name: 'Sophia',
+    breed: 'Dog - Shih tzu',
+    src: './assets/image/our-pets/Sophia.png',
+    text: 'Sophia here and I`m looking for my forever home to live out the best years of my life. I am full of energy. Everyday I`m learning new things, like how to walk on a leash, go potty outside, bark and play with toys and I still need some practice.',
+    info: {
+      age: '2 months',
+      inoculations: 'none',
+      diseases: 'none',
+      parasites: 'none'
+    }
+  },
+
+  {
+    name: 'Timmy',
+    breed: 'Cat - British Shorthair',
+    src: './assets/image/our-pets/Timmy.png',
+    text: 'Timmy is an adorable grey british shorthair male. He loves to play and snuggle. He is neutered and up to date on age appropriate vaccinations. He can be chatty and enjoys being held. Timmy has a lot to say and wants a person to share his thoughts with.',
+    info: {
+      age: '6 months',
+      inoculations: 'all',
+      diseases: 'none',
+      parasites: 'none'
+    }
+  },
+
+  {
+    name: 'Charly',
+    breed: 'Dog - Jack Russell Terrier ',
+    src: './assets/image/our-pets/Charly.png',
+    text: 'This cute boy, Charly, is three years old and he likes adults and kids. He isn’t fond of many other dogs, so he might do best in a single dog home. Charly has lots of energy, and loves to run and play. We think a fenced yard would make him very happy.',
+    info: {
+      age: '3 years old',
+      inoculations: 'all',
+      diseases: 'none',
+      parasites: 'none'
+    }
+  },
+
+  {
+    name: 'Scarlett',
+    breed: 'Dog - Jack Russell Terrier',
+    src: './assets/image/our-pets/Scarlett.png',
+    text: 'Scarlett is a happy, playful girl who will make you laugh and smile. She forms a bond quickly and will make a loyal companion and a wonderful family dog or a good companion for a single individual too since she likes to hang out and be with her human.',
+    info: {
+      age: '3.5 months',
+      inoculations: 'all',
+      diseases: 'none',
+      parasites: 'none'
+    }
+  },
+
+  {
+    name: 'Freddie',
+    breed: 'Cat - British Shorthair',
+    src: './assets/image/our-pets/Freddie.png',
+    text: 'Timmy is an adorable grey british shorthair male. He loves to play and snuggle. He is neutered and up to date on age appropriate vaccinations. He can be chatty and enjoys being held. Timmy has a lot to say and wants a person to share his thoughts with.',
+    info: {
+      age: '1 year old',
+      inoculations: 'all',
+      diseases: 'none',
+      parasites: 'none'
+    }
+  },
+
+  {
+    name: 'Sayko',
+    breed: 'Cat - British Shorthair',
+    src: './assets/image/our-pets/Sayko.png',
+    text: 'Sayko alittle princess.',
+    info: {
+      age: '1.5 month',
+      inoculations: 'none',
+      diseases: 'none',
+      parasites: 'none'
+    }
+  }
+
+];
