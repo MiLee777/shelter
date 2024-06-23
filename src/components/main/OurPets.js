fca7b975e329ec3d25b60c5a9b908463bdaf6dc9
MiLee777 <midyashka1506@gmail.com>
2024-06-23 15:14:34 +0700

    feat: add slider rendering

--- src/components/main/OurPets.js
+++ src/components/main/OurPets.js
@@ -1,5 +1,34 @@
+import { Pets } from '../../data';
+import ArrowLeft from '../../assets/image/icons/arrow-left.svg';
+import ArrowRight from '../../assets/image/icons/arrow-right.svg';
+
 export function OurPets() {
-  return <div>
-    Our Pets
+  return <div className='pets'>
+    <div className='container pets__container'>
+      <h2 className='title pets__title'>Our friends who <br/> are looking for a house</h2>
+      <div className='pets__slider'>
+      <button className='pets__slider-btn'><img src={ ArrowLeft } alt='Arrow' /></button>
+
+        {Pets.map((pet, index) => {
+          const { name, src } = pet;
+
+          return (
+            <div key={ index } className='pets__content'>
+              <div className='pets__box'>
+                <img src={ src } alt='Pet' />
+                <div className='pet__info'>
+                  <p className='pets__name'>{ name }</p>
+                  <button className='pets__box-btn'>Learn more</button>
+                </div>
+              </div>
+            </div>
+          )
+        })}
+
+
+      <button className='pets__slider-btn'><img src={ ArrowRight } alt='Arrow' /></button>
+      </div>
+
+    </div>
   </div>
 }
--- src/data.js
+++ src/data.js
@@ -1,8 +1,18 @@
-export const Pets = () => [
+import Katrine from './assets/image/our-pets/Katrine.png';
+import Jeniffer from './assets/image/our-pets/Jeniffer.png';
+import Woody from './assets/image/our-pets/Woody.png';
+import Sophia from './assets/image/our-pets/Sophia.png';
+import Timmy from './assets/image/our-pets/Timmy.png';
+import Charly from './assets/image/our-pets/Charly.png';
+import Scarlett from './assets/image/our-pets/Scarlett.png';
+import Freddie from './assets/image/our-pets/Freddie.png';
+import Sayko from './assets/image/our-pets/Sayko.avif';
+
+export const Pets = [
 
   {
     name: 'Katrine',
-    src: './assets/image/our-pets/Katrine.png',
+    src: Katrine,
     breed: 'Cat - British Shorthair',
     text: 'Scarlett is a happy, playful girl who will make you laugh and smile. She forms a bond quickly and will make a loyal companion and a wonderful family dog or a good companion for a single individual too since she likes to hang out and be with her human.',
     info: {
@@ -16,7 +26,7 @@ export const Pets = () => [
   {
     name: 'Jennifer',
     breed: 'Dog - Labrador',
-    src: './assets/image/our-pets/Jennifer.png',
+    src: Jeniffer,
     text: 'Jennifer is a sweet 2 months old Labrador that is patiently waiting to find a new forever home. This girl really enjoys being able to go outside to run and play, but won`t hesitate to play up a storm in the house if she has all of her favorite toys.',
     info: {
       age: '2 months',
@@ -29,7 +39,7 @@ export const Pets = () => [
   {
     name: 'Woody',
     breed: 'Dog - Golden Retriever',
-    src: './assets/image/our-pets/Woody.png',
+    src: Woody,
     text: 'Woody is a handsome 3 1/2 years old boy. Woody does know basic commands and is a smart pup. Since he is on the stronger side, he will learn a lot from your training. Woody will be happier when he finds a new family that can spend a lot of time with him.',
     info: {
       age: '3.5 years old',
@@ -42,7 +52,7 @@ export const Pets = () => [
   {
     name: 'Sophia',
     breed: 'Dog - Shih tzu',
-    src: './assets/image/our-pets/Sophia.png',
+    src: Sophia,
     text: 'Sophia here and I`m looking for my forever home to live out the best years of my life. I am full of energy. Everyday I`m learning new things, like how to walk on a leash, go potty outside, bark and play with toys and I still need some practice.',
     info: {
       age: '2 months',
@@ -55,7 +65,7 @@ export const Pets = () => [
   {
     name: 'Timmy',
     breed: 'Cat - British Shorthair',
-    src: './assets/image/our-pets/Timmy.png',
+    src: Timmy,
     text: 'Timmy is an adorable grey british shorthair male. He loves to play and snuggle. He is neutered and up to date on age appropriate vaccinations. He can be chatty and enjoys being held. Timmy has a lot to say and wants a person to share his thoughts with.',
     info: {
       age: '6 months',
@@ -68,7 +78,7 @@ export const Pets = () => [
   {
     name: 'Charly',
     breed: 'Dog - Jack Russell Terrier ',
-    src: './assets/image/our-pets/Charly.png',
+    src: Charly,
     text: 'This cute boy, Charly, is three years old and he likes adults and kids. He isn’t fond of many other dogs, so he might do best in a single dog home. Charly has lots of energy, and loves to run and play. We think a fenced yard would make him very happy.',
     info: {
       age: '3 years old',
@@ -81,7 +91,7 @@ export const Pets = () => [
   {
     name: 'Scarlett',
     breed: 'Dog - Jack Russell Terrier',
-    src: './assets/image/our-pets/Scarlett.png',
+    src: Scarlett,
     text: 'Scarlett is a happy, playful girl who will make you laugh and smile. She forms a bond quickly and will make a loyal companion and a wonderful family dog or a good companion for a single individual too since she likes to hang out and be with her human.',
     info: {
       age: '3.5 months',
@@ -94,7 +104,7 @@ export const Pets = () => [
   {
     name: 'Freddie',
     breed: 'Cat - British Shorthair',
-    src: './assets/image/our-pets/Freddie.png',
+    src: Freddie,
     text: 'Timmy is an adorable grey british shorthair male. He loves to play and snuggle. He is neutered and up to date on age appropriate vaccinations. He can be chatty and enjoys being held. Timmy has a lot to say and wants a person to share his thoughts with.',
     info: {
       age: '1 year old',
@@ -107,7 +117,7 @@ export const Pets = () => [
   {
     name: 'Sayko',
     breed: 'Cat - British Shorthair',
-    src: './assets/image/our-pets/Sayko.png',
+    src: Sayko,
     text: 'Sayko alittle princess.',
     info: {
       age: '1.5 month',
